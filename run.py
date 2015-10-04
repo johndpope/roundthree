84c83f02d45152866920cfa26c5a271be4bd400d
Joseph Min <jmin9@jhu.edu>
2015-10-03 20:03:43 -0400

    include Node

--- run.py
+++ run.py
@@ -1,18 +1,19 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+from Node import Node
  
 app = Flask(__name__)
 
 start_node = Node("Welcome to #thecloud. If you have not done so already, please call emergency services. Are you safe and able to assist? (y/n)")
-start_node.add_response("y", """air""")
-start_node.add_response("n", """Leave""")
 air_node = Node("Look in your patient's mouth. Are there any obstructions? If there are, remove it if possible. Is the airway now open? (y/n)")
-air_node.add_response("y", """B""")
-air_node.add_response("n", """heimlich""")
 heimlich_node = Node("Perform the Heimlich maneuver: Get behind your patient. Place your fist with your thumb in and facing their belly button. Grasping your fist with your other hand, make quick, upward and inward thrusts with your fist.")
-heimlich_node.add_response("y", """B""")
-heimlich_node.add_response("n", """h-o CPR""")
 breathing_node = Node("Is your patient breathing?")
+# heimlich_node.add_response("y", """B""")
+# heimlich_node.add_response("n", """h-o CPR""")
+start_node.add_response("y", air_node)
+start_node.add_response("n", air_node)
+# air_node.add_response("y", """B""")
+# air_node.add_response("n", """heimlich""")
 curr_node = start_node
 
 
