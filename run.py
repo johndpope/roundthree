6a4c42ff58dd1cf3844d382cf552023c6fa271c3
Joseph Min <jmin9@jhu.edu>
2015-10-03 23:03:18 -0400

    run.py

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import twilio.twiml
 from Node import Node
  
 app = Flask(__name__)
-print("testteststseteats")
 start_node = None
 air_node = None
 curr_node = None
@@ -14,11 +13,9 @@ def hello_monkey():
     """Respond to incoming calls with a simple text message."""
     global curr_node
     resp = twilio.twiml.Response()
-    if (curr_node is start_node):
-    	resp.message("Welcome to #thecloud. If you have not done so already, please call emergency services. Are you safe and able to assist? (y/n)")
-    	curr_node = air_node
-    elif (curr_node == air_node):
-    	resp.message(air_node.get_message())
+    if (curr_node != start_node):
+        curr_node = curr_node.get_next_node()
+    resp.message(curr_node.get_message())
 
     return str(resp)
  
@@ -27,12 +24,21 @@ if __name__ == "__main__":
     start_node = Node("Welcome to #thecloud. If you have not done so already, please call emergency services. Are you safe and able to assist? (y/n)")
     air_node = Node("Look in your patient's mouth. Are there any obstructions? If there are, remove it if possible. Is the airway now open? (y/n)")
     heimlich_node = Node("Perform the Heimlich maneuver: Get behind your patient. Place your fist with your thumb in and facing their belly button. Grasping your fist with your other hand, make quick, upward and inward thrusts with your fist.")
-    breathing_node = Node("Is your patient breathing?")
-# heimlich_node.add_response("y", """B""")
-# heimlich_node.add_response("n", """h-o CPR""")
+    breathing_node = Node("Is your patient breathing? (y/n)")
+    mtm_node = Node("If you have the ability to do so safely, administer rescue breaths at a rate of two breaths per minute.")
+    circulation_node = Node("Does your patient have a pulse? (y/n)")
+    cpr_node = Node("Administer hands-only CPR at a rate of 100 beats per minute. This is approximately the beat to the hit song \"Staying Alive\"")
+    dense_bleed_node = Node("Does your patient have a dense bleed? Check for large pools of blood. (y/n)")
+
+
+	# heimlich_node.add_response("y", """""")
+	# heimlich_node.add_response("n", """h-o CPR""")
     start_node.add_response("y", air_node)
     start_node.add_response("n", air_node)
-# air_node.add_response("y", """B""")
-# air_node.add_response("n", """heimlich""")
+    air_node.add_response("y", breathing_node)
+    air_node.add_response("n", heimlich_node)
+    breathing_node.add_response("y", circulation_node)
+    breathing_node.add_response("n", mtm_node)
+    circulation_node.add_response("y", dense_bleed_node)
     curr_node = start_node
     app.run(debug=True, host='0.0.0.0')
