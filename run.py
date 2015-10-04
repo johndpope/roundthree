5a862db8852680e108c794f7be1cfd7410648b6b
Ubuntu <ubuntu@ip-172-31-60-208.ec2.internal>
2015-10-04 02:42:04 +0000

    super prototype

--- run.py
+++ run.py
@@ -3,17 +3,18 @@ import twilio.twiml
 from Node import Node
  
 app = Flask(__name__)
-
+print("testteststseteats")
 start_node = None
 air_node = None
+curr_node = None
 
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
- 
+    global curr_node
     resp = twilio.twiml.Response()
-    if (curr_node == start_node):
+    if (curr_node is start_node):
     	resp.message("Welcome to #thecloud. If you have not done so already, please call emergency services. Are you safe and able to assist? (y/n)")
     	curr_node = air_node
     elif (curr_node == air_node):
@@ -23,15 +24,15 @@ def hello_monkey():
  
 if __name__ == "__main__":
 
-    app.run(debug=True, host='0.0.0.0')
     start_node = Node("Welcome to #thecloud. If you have not done so already, please call emergency services. Are you safe and able to assist? (y/n)")
-	air_node = Node("Look in your patient's mouth. Are there any obstructions? If there are, remove it if possible. Is the airway now open? (y/n)")
-	heimlich_node = Node("Perform the Heimlich maneuver: Get behind your patient. Place your fist with your thumb in and facing their belly button. Grasping your fist with your other hand, make quick, upward and inward thrusts with your fist.")
-	breathing_node = Node("Is your patient breathing?")
+    air_node = Node("Look in your patient's mouth. Are there any obstructions? If there are, remove it if possible. Is the airway now open? (y/n)")
+    heimlich_node = Node("Perform the Heimlich maneuver: Get behind your patient. Place your fist with your thumb in and facing their belly button. Grasping your fist with your other hand, make quick, upward and inward thrusts with your fist.")
+    breathing_node = Node("Is your patient breathing?")
 # heimlich_node.add_response("y", """B""")
 # heimlich_node.add_response("n", """h-o CPR""")
-	start_node.add_response("y", air_node)
-	start_node.add_response("n", air_node)
+    start_node.add_response("y", air_node)
+    start_node.add_response("n", air_node)
 # air_node.add_response("y", """B""")
 # air_node.add_response("n", """heimlich""")
-	curr_node = start_node
+    curr_node = start_node
+    app.run(debug=True, host='0.0.0.0')
