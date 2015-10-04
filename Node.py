9e2e31734d581e9a76277b7eda7a6191feabb14b
Joseph Min <jmin9@jhu.edu>
2015-10-03 20:07:56 -0400

    more changes

--- Node.py
+++ Node.py
@@ -4,13 +4,13 @@ class Node:
         self.message = message
         self.responses = {}
 
-    def add_response(input, new_node):
+    def add_response(self, input, new_node):
         self.responses[input] = new_node
 
-    def get_message():
+    def get_message(self):
         return self.message
 
-    def get_next_node(input):
+    def get_next_node(self, input):
         if (self.responses):
             return self.responses[input]
         else:
--- run.py
+++ run.py
@@ -4,17 +4,8 @@ from Node import Node
  
 app = Flask(__name__)
 
-start_node = Node("Welcome to #thecloud. If you have not done so already, please call emergency services. Are you safe and able to assist? (y/n)")
-air_node = Node("Look in your patient's mouth. Are there any obstructions? If there are, remove it if possible. Is the airway now open? (y/n)")
-heimlich_node = Node("Perform the Heimlich maneuver: Get behind your patient. Place your fist with your thumb in and facing their belly button. Grasping your fist with your other hand, make quick, upward and inward thrusts with your fist.")
-breathing_node = Node("Is your patient breathing?")
-# heimlich_node.add_response("y", """B""")
-# heimlich_node.add_response("n", """h-o CPR""")
-start_node.add_response("y", air_node)
-start_node.add_response("n", air_node)
-# air_node.add_response("y", """B""")
-# air_node.add_response("n", """heimlich""")
-curr_node = start_node
+start_node = None
+air_node = None
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -33,3 +24,14 @@ def hello_monkey():
 if __name__ == "__main__":
 
     app.run(debug=True, host='0.0.0.0')
+    start_node = Node("Welcome to #thecloud. If you have not done so already, please call emergency services. Are you safe and able to assist? (y/n)")
+	air_node = Node("Look in your patient's mouth. Are there any obstructions? If there are, remove it if possible. Is the airway now open? (y/n)")
+	heimlich_node = Node("Perform the Heimlich maneuver: Get behind your patient. Place your fist with your thumb in and facing their belly button. Grasping your fist with your other hand, make quick, upward and inward thrusts with your fist.")
+	breathing_node = Node("Is your patient breathing?")
+# heimlich_node.add_response("y", """B""")
+# heimlich_node.add_response("n", """h-o CPR""")
+	start_node.add_response("y", air_node)
+	start_node.add_response("n", air_node)
+# air_node.add_response("y", """B""")
+# air_node.add_response("n", """heimlich""")
+	curr_node = start_node
